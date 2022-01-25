757fbc4c4f0c13b10479f07ab6a6bb9533a253ef
Andrew Palmer <atp@sdf.org>
2022-01-25 04:02:28 -0600

    Separate insertion and selection sorts

--- insertion/__main__.py
+++ insertion/__main__.py
@@ -4,13 +4,6 @@ import random
 DATA = [random.randint(1, 101) for _ in range(10)]
 
 
-def selection_sort(items):
-    for curr in range(len(items)):
-        for seek in range(curr + 1, len(items)):
-            if items[seek] < items[curr]:
-                items[curr], items[seek] = items[seek], items[curr]
-
-
 def insertion_sort(items):
     for seek in range(1, len(items)):
         item = items[seek]
@@ -25,10 +18,6 @@ def insertion_sort(items):
 
 def main():
     print(DATA)
-
-    data = DATA.copy()
-    selection_sort(data)
-    print(data)
 
     data = DATA.copy()
     insertion_sort(data)
